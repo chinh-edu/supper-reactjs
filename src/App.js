1b5a1f74b881cefe8ec301dfe1b87e9f2e9fe5ed
Chi Nguyen <nguyenhuuchi19942606@gmail.com>
2023-04-10 23:02:14 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+//Availability
+import logo from './logo.svg';
+import './App.css';
+
+// Add library
+
+
+
+// Add file
+
+
+const App = () => {
+
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Hello world reactjs</h3>
+      </header>
+
+    </div >
+
+  );
+}
+
+export default App; 
