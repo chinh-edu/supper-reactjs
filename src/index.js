7429b650fc0361213e1067d0e4772ffea1ba3af1
Chi Nguyen <nguyenhuuchi19942606@gmail.com>
2023-05-08 23:23:03 +0700

    redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import Layout from './Layout';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  // <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <Layout />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-
+  </Provider>
+  // </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
